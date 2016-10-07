bcb1811083fa0017ad4bfce3aa933aab92603241
Austin Akers <austin.akers5@gmail.com>
2016-10-06 23:09:03 -0500

    Initial parts of games

---
+++ CanvasGame/game.js
@@ -0,0 +1,43 @@
+var canvasBg = document.getElementById("canvasBg"),
+    ctxBg = canvasBg.getContext("2d"),
+    canvasEntities = document.getElementById("canvasEntities"),
+    ctxEntities = canvasEntities.getContext("2d"),
+    canvasWidth = canvasBg.width,
+    canvasHeight = canvasBg.height,
+    //player1 = new Player(),
+    //enemies = [],
+    //numEnemies = 5,
+    //obstacles = [],
+    isPlaying = false,
+    requestAnimFrame =  window.requestAnimationFrame ||
+                        window.webkitRequestAnimationFrame ||
+                        window.mozRequestAnimationFrame ||
+                        window.oRequestAnimationFrame ||
+                        window.msRequestAnimationFrame ||
+                        function(callback) {
+                            window.setTimeout(callback, 1000 / 60);
+                        },
+    imgSprite = new Image();
+imgSprite.src = "images/sprite.png";
+imgSprite.addEventListener("load", init, false);
+
+
+function init() {
+    //document.addEventListener("keydown", checkKeyDown, false);
+    //document.addEventListener("keyup", checkKeyUp, false);
+    //defineObstacles();
+    //initEnemies();
+    begin();
+}
+
+function begin() {
+    ctxBg.drawImage(imgSprite, 0, 0, canvasWidth, canvasHeight, 0, 0, canvasWidth, canvasHeight);
+    isPlaying = true;
+    requestAnimFrame(loop);
+}
+
+function loop() {
+
+}
+
+
