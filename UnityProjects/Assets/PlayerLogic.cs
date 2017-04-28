e284f37961178bb599e39c348dbb3585b3661015
Austin Akers <austin.akers5@gmail.com>
2017-04-27 23:59:15 -0500

    Added projectile Logic

---
+++ UnityProjects/Assets/PlayerLogic.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour {
+
+    public GameObject projectile;
+
+    public float playerSpeed = 1.0f;
+    public float shootingSpeed = 0.5f;
+
+    float maxPlayerXDistance = 30.0f;
+
+    public bool isShooting = true;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(projectileShootTimer());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerMovemment();	
+	}
+
+    void playerMovemment() {
+        if (Input.GetKey(KeyCode.A) && transform.position.x >= -maxPlayerXDistance)
+        {
+            transform.Translate(-playerSpeed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D) && transform.position.x <= maxPlayerXDistance)
+        {
+            transform.Translate(playerSpeed, 0, 0);
+        }
+    }
+
+    IEnumerator projectileShootTimer()
+    {
+        while (isShooting == true)
+        {
+            spawnProjectile();
+            yield return new WaitForSeconds(shootingSpeed);
+        }
+    }
+
+    void spawnProjectile()
+    {
+        Instantiate(projectile, transform.position, Quaternion.identity);
+    }
+}
---
+++ UnityProjects/Assets/ProjectileLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLogic : MonoBehaviour {
+
+    public float transformSpeed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveTransformToTop();
+	}
+
+    void moveTransformToTop()
+    {
+        transform.Translate(0, transformSpeed, 0);
+    }
+}
