cfa4bec5dcaf978e3c4e529c1f997c038309e95a
Austin Akers <austin.akers5@gmail.com>
2017-05-03 22:25:06 -0500

    Created base functionality for enemy AI/Spawn Behavior

---
+++ UnityProjects/gamePrototype1/Assets/EnemySpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyToSpawn;
+
+    public bool canSpawn = true;
+
+    public float enemySpawnTime = 1.0f;
+
+    public float maxX = 28.0f;
+
+    public float startingY = 20.0f;
+
+    float randomX = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(spawnEnemyTimer());
+	}
+
+    IEnumerator spawnEnemyTimer()
+    {
+        spawnEnemy();
+        yield return new WaitForSeconds(enemySpawnTime);
+    }
+
+    void spawnEnemy()
+    {
+        randomX = Random.Range(maxX, maxX);
+        Instantiate(enemyToSpawn, new Vector3(randomX, startingY, 0), Quaternion.identity);
+    }
+
+}
---
+++ UnityProjects/gamePrototype1/Assets/moveTransformDown.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTransformDown : MonoBehaviour {
+
+    public float enemySpeed = 0.1f;
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        moveToBottom();
+		
+	}
+
+    void moveToBottom()
+    {
+        transform.Translate(0, -tranformSpeed, 0);
+    }
+}
